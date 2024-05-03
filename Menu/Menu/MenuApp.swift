5dc889a5309a6c1831048102d8040b99861bdd08
HNG90 <hirenngandhi@gmail.com>
2024-05-03 18:03:11 -0400

    Add files via upload

---
+++ Menu/Menu/MenuApp.swift
@@ -0,0 +1,17 @@
+//
+//  MenuApp.swift
+//  Menu
+//
+//  Created by Hiren Gandhi on 6/23/23.
+//
+
+import SwiftUI
+
+@main
+struct MenuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MenuView()
+        }
+    }
+}
